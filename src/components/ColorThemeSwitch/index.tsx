c641b7d44c4b04b2bdc6b60ee3cd45c1fa706329
yuko <yuriii.kovalyov@gmail.com>
2023-01-18 01:53:22 +0300

    Added CssVarsProvider and color scheme switcher.

---
+++ src/components/ColorThemeSwitch/index.tsx
@@ -0,0 +1,47 @@
+import {
+  Typography,
+  ToggleButtonGroup,
+  ToggleButton,
+  useColorScheme,
+} from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+export default function ColorThemeSwitch() {
+  const { colorScheme, setColorScheme } = useColorScheme();
+
+  return (
+    <ToggleButtonGroup
+      value={colorScheme}
+      size="small"
+      exclusive
+      aria-label="Switch color scheme"
+      onChange={(e, value) => {
+        setColorScheme(value);
+      }}
+    >
+      <ToggleButton
+        value="dark"
+        key="left"
+        aria-label="Dark mode"
+        title="Dark mode"
+      >
+        <DarkModeIcon color="inherit" />
+      </ToggleButton>
+
+      {/* <ToggleButton value="system" key="system">
+        <SettingsBrightnessIcon />
+      </ToggleButton> */}
+
+      <ToggleButton
+        value="light"
+        key="light"
+        aria-label="Light mode"
+        title="Light mode"
+      >
+        <LightModeIcon color="inherit" />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,61 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import {
-  createTheme,
-  ThemeProvider,
-  StyledEngineProvider,
+  responsiveFontSizes,
+  Experimental_CssVarsProvider,
+  experimental_extendTheme,
 } from "@mui/material/styles";
-import StylesProvider from "@mui/styles/StylesProvider";
+import type {} from "@mui/material/themeCssVarsAugmentation";
+import CssBaseline from "@mui/material/CssBaseline";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
 import { createStore } from "./app/store";
 
-const theme = createTheme({
-  palette: {
-    secondary: {
-      main: "#fff",
+import "./index.css";
+
+const theme = responsiveFontSizes(
+  experimental_extendTheme({
+    colorSchemes: {
+      dark: {
+        palette: {
+          primary: {
+            main: "#000",
+          },
+        },
+      },
+      light: {
+        palette: {
+          background: {
+            default: "#f0f2f4",
+          },
+          primary: {
+            main: "#ff9800",
+          },
+        },
+      },
     },
-  },
-});
+    typography: {
+      fontSize: 16,
+    },
+  }),
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 const store = createStore();
 
 root.render(
-  // <React.StrictMode>
   <Provider store={store}>
-    <StylesProvider injectFirst>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <DndProvider backend={HTML5Backend}>
-              <App />
-            </DndProvider>
-          </BrowserRouter>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </StylesProvider>
-  </Provider>
-  // </React.StrictMode>
+    <Experimental_CssVarsProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <BrowserRouter basename="/">
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </BrowserRouter>
+    </Experimental_CssVarsProvider>
+  </Provider>,
 );
