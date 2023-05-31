599aab1ce406a7181bd2dfadb04fafcbf7992cf8
yuko <yuriii.kovalyov@gmail.com>
2023-05-31 14:02:06 +0300

    Refactored vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [react(), tsconfigPaths(), svgr()],
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      checker({ typescript: true }),
+      visualizer(),
+    ],
     base: env.VITE_PUBLIC_URL || "/",
+    build: {
+      target: "modules",
+      modulePreload: false,
+    },
     server: {
+      port: 3000,
+      host: env.VITE_HOST || "127.0.0.1",
+    },
+    preview: {
       port: 3000,
       host: env.VITE_HOST || "127.0.0.1",
     },
